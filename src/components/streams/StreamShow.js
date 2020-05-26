b3da0831bbee20a61b1cfc88862ea87811ef64d7
pcgamer1 <saxenasarthak72@gmail.com>
2020-05-26 19:27:29 +0530

    Bug fixes

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
+import flv from 'flv.js'
 
 class StreamShow extends React.Component {
 
+    constructor(props) {
+        super(props)
+
+        this.videoRef = React.createRef()
+    }
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id)
+        const { id } = this.props.match.params
+        this.props.fetchStream(id)
+        this.buildPlayer()
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer()
+    }
+
+    buildPlayer() {
+        const { id } = this.props.match.params
+        if(this.player || !this.props.stream)
+            return
+            this.player = flv.createPlayer({
+                type: 'flv',
+                url: `http://127.0.0.1:8000/live/${id}.flv`
+            })
+            this.player.attachMediaElement(this.videoRef.current)
+            this.player.load()
     }
 
     render() {
@@ -13,6 +38,7 @@ class StreamShow extends React.Component {
         const {title, description} = this.props.stream
         return (
             <div>
+                <video ref={this.videoRef} style={{width:'100%'}} controls/>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
